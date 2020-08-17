7a54d907abc9c68fa002ad5935be1a7c10381a29
Ali Gamal Ali <hictors@gmail.com>
2020-08-17 16:59:32 +0200

    doing a post request for a link

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -5,4 +5,32 @@ toggler.addEventListener("click", () => {
   toggler.classList.toggle("rotate");
 });
 
+let shortenLinks= [];
+let originalURL = "";
+
+let myURL = `https://www.facebook.com/Arigatouz?ref=bookmarks/`;
+const shortenAPILink = `https://rel.ink/api/links/`;
+const fetchLINK = async (method, link) => {
+  const response = await fetch(shortenAPILink, {
+    method: method,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: "same-origin",
+    body: JSON.stringify({ url: link }),
+  });
+  const jsonData = await response.json(link);
+  console.log(jsonData.url);
+  console.log(jsonData.hashid);
+  console.log(shortenAPILink + jsonData.hashid);
+  return {
+    url: `${shortenAPILink}${jsonData.hashid}`,
+    originalURL: jsonData.url,
+  };
+};
+
+fetchLINK("POST", myURL)
+
+
 
