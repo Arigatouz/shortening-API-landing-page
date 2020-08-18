6e9892210f794d915c4a523c424da0b3c112bca3
Ali Gamal Ali <hictors@gmail.com>
2020-08-18 06:37:22 +0200

    first try on posting and geting links

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,18 +1,25 @@
 const toggler = document.getElementById("menuToggler");
 const navMenu = document.getElementById("navMenu");
+const enteredLink = document.querySelector(".result__insertedLink");
+const outPutLink = document.querySelector(".result__outPutLink p");
 toggler.addEventListener("click", () => {
   navMenu.classList.toggle("toggle");
   toggler.classList.toggle("rotate");
 });
+const form = document.querySelector("form");
+const myURL = document.getElementById("shortenLinks__input");
 
-let shortenLinks= [];
-let originalURL = "";
-
-let myURL = `https://www.facebook.com/Arigatouz?ref=bookmarks/`;
+const shortenLinks = [];
+let shortenLink = {
+  id: Math.random(),
+  link: "",
+  shortenedLink: "",
+};
 const shortenAPILink = `https://rel.ink/api/links/`;
-const fetchLINK = async (method, link) => {
+
+const fetchLINK = async (link) => {
   const response = await fetch(shortenAPILink, {
-    method: method,
+    method: "POST",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
@@ -20,17 +27,33 @@ const fetchLINK = async (method, link) => {
     credentials: "same-origin",
     body: JSON.stringify({ url: link }),
   });
-  const jsonData = await response.json(link);
-  console.log(jsonData.url);
-  console.log(jsonData.hashid);
-  console.log(shortenAPILink + jsonData.hashid);
-  return {
-    url: `${shortenAPILink}${jsonData.hashid}`,
-    originalURL: jsonData.url,
-  };
+  try {
+    const jsonData = await response.json(link);
+    shortenLink.link = jsonData.url;
+    shortenLink.shortenedLink = `${shortenAPILink}${jsonData.hashid}`;
+    shortenLinks.push(shortenLink);
+    const getLink = async () => {
+      const result = await fetch(shortenLink.shortenedLink);
+      try {
+        const resultJson = await result.json();
+        enteredLink.textContent = shortenLink.link;
+        outPutLink.textContent = shortenLink.shortenedLink;
+        shortenLink.shortenedLink = document.location.href = shortenLink.link;
+        console.log(resultJson);
+        console.log(shortenLink.shortenedLink);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getLink();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-fetchLINK("POST", myURL)
-
-
-
+form.addEventListener("submit", (e) => {
+  fetchLINK(myURL.value);
+  e.preventDefault();
+  myURL.value = "";
+});
